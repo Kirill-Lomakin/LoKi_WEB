89cfae4a752c605d322316daed3fd5a208b13b55
Kirill-Lomakin <47152842+Kirill-Lomakin@users.noreply.github.com>
2022-01-25 18:10:19 +0300

    V0.2

---
+++ post.php
@@ -0,0 +1,52 @@
+<?
+ 
+if(isset($_POST["name"]))
+{
+        if(isset($_POST["name"]))
+        {
+                $name = $_POST["name"];
+        }
+        if(isset($_POST["phone"]))
+        {
+                $phone= $_POST["phone"];
+        }
+        if(isset($_POST["email"]))
+        {
+                $email = $_POST["email"];
+        }
+ 
+        if($name=="" or $phone=="" or $email=="")
+        { // Проверяем на заполненность всех полей.
+                echo "Пожалуйста, заполните все поля";
+        }
+        else
+        {
+                $ip=$_SERVER["REMOTE_ADDR"]; // Вычисляем ip пользователя
+                $brose=$_SERVER["HTTP_USER_AGENT"]; // Вычисляем браузер пользователя
+                $to = "citrolab@yandex.ru"; // Ваш email адрес
+                $subject = "Сообщение c сайта LoKi_WEB"; // тема письма
+                $headers .= "Content-Type: text/html;
+                ";
+                $headers .= "Отправитель: Посетитель сайта"; // Отправитель письма
+                $message = "
+                Имя: $name<br>
+                Телефоны: $phone<br>
+                Почта: $email<br><br>
+ 
+                IP отправителя: $ip<br>
+                Браузер отправителя: $brose<br>
+                ";
+ 
+                $send = mail($to, $subject, $message, $headers);
+                if ($send == "true")
+                {
+                        echo "Ваше сообщение отправлено. Я отвечу Вам в ближайшее время.";
+                }
+                else
+                {
+                        echo "Не удалось отправить, попробуйте снова!";
+                }
+        }
+}
+ 
+?>
